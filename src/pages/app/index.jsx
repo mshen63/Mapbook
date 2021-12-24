68b63bcfa81cd03d3e81d9f2db6abef2a6385d2a
mshen63 <mshen63@gatech.edu>
2021-12-23 20:58:06 -0500

    Search for location bar, Mapbox API

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -5,67 +5,13 @@ import "leaflet/dist/leaflet.css"
 import { getCurrentUser } from "../../actions/User";
 const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js")
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_KEY
+import MapScreen from "../../screens/App/Map/MapScreen";
 
-const MapPage = ({ currUser, pageProps }) => {
-    const [pageIsMounted, setPageIsMounted] = useState(false);
-    const [Map, setMap] = useState();
+const MapPage = ({ currUser, pageProps }) => (
+        <MapScreen />
 
-    useEffect(() => {
-        setPageIsMounted(true);
-    }, [])
-    useEffect(() => {
-
-        if (pageIsMounted) {
-            const map = new mapboxgl.Map({
-                container: "my-map",
-                style: "mapbox://styles/mapbox/streets-v11",
-                center: [-77.02, 38.887],
-                zoom: 12.5,
-                pitch: 45,
-                maxBounds: [
-                    [-77.875588, 38.50705], // Southwest coordinates
-                    [-76.15381, 39.548764], // Northeast coordinates
-                ],
-            });
-
-            
-            map.addControl(
-                new mapboxgl.GeolocateControl({
-                    positionOptions: {
-                        enableHighAccuracy: true,
-                    },
-                    trackUserLocation: true
-                })
-            )
-            const container = map.getContainer();
-            let relocateB = container.getElementsByClassName('mapboxgl-ctrl-geolocate')
-            Array.prototype.slice.call(relocateB).forEach((button)=>button.click())
-
-        }
-
-    }, [pageIsMounted]);
-
-    // useEffect(() => {
-    //     if (pageIsMounted && data) {
-    //         Map.on("load", function () {
-    //             addDataLayer(Map, data);
-    //         });
-    //     }
-    // }, [pageIsMounted, setMap, data, Map]);
-
-    return (
-        <div >
-
-            <button id="Hello" onClick={() => console.log("clicked!")}></button>
-            <main >
-                <div id="my-map" style={{ height: 500 }} />
-            </main>
-
-
-        </div>
     );
 
-}
 
 MapPage.getInitialProps = async ({ req, res, pageProps }) => {
     console.log(pageProps)
--- src/screens/App/Map/MapScreen.jsx
+++ src/screens/App/Map/MapScreen.jsx
@@ -4,37 +4,75 @@ import urls from "../../../../utils/urls";
 import classes from "./MapScreen.module.css";
 import React, { useState, useEffect, useContext } from 'react';
 // import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents } from 'react-leaflet';
-import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import ReactMapGL from 'react-mapbox-gl'
+// import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+// import ReactMapGL from 'react-mapbox-gl'
+const mapboxgl = require("mapbox-gl/dist/mapbox-gl.js")
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_KEY
+
 
 const MapScreen = ({ currUser }) => {
-    const [viewport, setViewport] = useState({
-        width: "100%",
-        height: "100%",
-        // The latitude and longitude of the center of London
-        latitude: 51.5074,
-        longitude: -0.1278,
-        zoom: 10
-    });
-    return <ReactMapGL
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        mapboxApiAccessToken={process.env.MAPBOX_KEY}
-        {...viewport}
-        onViewportChange={(nextViewport) => setViewport(nextViewport)}
-    >
-        {/* {locations.map((location) => (
-            <div key={location.id}>
-                <Marker
-                    latitude={location.center[1]}
-                    longitude={location.center[0]}
-                    offsetLeft={-20}
-                    offsetTop={-10}>
-                    <span role="img" aria-label="push-pin">📌</span>
-                </Marker>
-            </div>
-        ))} */}
-    </ReactMapGL>
+    const [pageIsMounted, setPageIsMounted] = useState(false);
+    const [Map, setMap] = useState();
+
+    useEffect(() => {
+        setPageIsMounted(true);
+    }, [])
+
+    useEffect(() => {
+
+        if (pageIsMounted) {
+            const map = new mapboxgl.Map({
+                container: "my-map",
+                style: "mapbox://styles/mapbox/streets-v11",
+                center: [-77.02, 38.887],
+                zoom: 5,
+                maxBounds: [
+                    [-77.875588, 38.50705], // Southwest coordinates
+                    [-76.15381, 39.548764], // Northeast coordinates
+                ],
+            });
+
+            map.addControl(
+                new MapboxGeocoder({
+                    accessToken: mapboxgl.accessToken, 
+                    mapboxgl: mapboxgl
+                })
+            )
+            map.addControl(
+                new mapboxgl.GeolocateControl({
+                    positionOptions: {
+                        enableHighAccuracy: true,
+                    },
+                    trackUserLocation: true
+                })
+            )
+
+            
+            const container = map.getContainer();
+            let relocateB = container.getElementsByClassName('mapboxgl-ctrl-geolocate')
+            Array.prototype.slice.call(relocateB).forEach((button)=>button.click())
+
+        }
+
+    }, [pageIsMounted]);
+
+    // useEffect(() => {
+    //     if (pageIsMounted && data) {
+    //         Map.on("load", function () {
+    //             addDataLayer(Map, data);
+    //         });
+    //     }
+    // }, [pageIsMounted, setMap, data, Map]);
+
+    return (
+        <div >
+            <main >
+                <div id="my-map" style={{ height: 500 }} />
+            </main>
+        </div>
+    );
+
 }
 
 
