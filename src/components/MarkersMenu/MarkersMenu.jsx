5e5d7726612a6ec410a03d8a4e1e731bc6ef5ea0
mshen63 <mshen63@gatech.edu>
2021-12-28 16:35:23 -0500

    Map page UI changes

--- src/components/MarkersMenu/MarkersMenu.jsx
+++ src/components/MarkersMenu/MarkersMenu.jsx
@@ -1,27 +1,33 @@
-import { Box, Stack, Button, Accordion, AccordionButton, AccordionPanel, AccordionItem, AccordionIcon } from "@chakra-ui/react";
+import { Text, Flex, Box, Stack, Button, Accordion, AccordionButton, AccordionPanel, AccordionItem, AccordionIcon, Divider } from "@chakra-ui/react";
 
 const MarkersMenu = (props) => {
     const { markers, map, setCurrMarker, setShowMenu } = props
+    console.log(props)
 
     return (
-        <Box
-
+        <Flex
             height="80vh"
             width="15vw"
             id="hellO"
             overflowY="scroll"
-
+            align="center"
+            direction="column"
         >
-
+            <Text>Markers: </Text>
+            <Divider borderColor="gray.600" marginTop={3} marginBottom={3} />
             {markers.map((mark) => {
                 return (
-                    <Stack margin={3} id = {mark}>
+                    <Stack margin="5px" key={mark._id} width="90%">
                         <Button onClick={(e) => {
                             map.flyTo({ center: [mark.lng, mark.lat], zoom: 8 })
                             setCurrMarker(mark)
                             setShowMenu(false)
                         }
-                        }>
+                        }
+                            bg="green.200"
+                            _hover={{ bg: "green.300" }}
+                            rounded={0}
+                        >
                             <Box flex='1' textAlign='left'>
                                 {mark.name}
                             </Box>
@@ -30,7 +36,7 @@ const MarkersMenu = (props) => {
                     </Stack>
                 )
             })}
-        </Box>
+        </Flex>
 
     )
 }
