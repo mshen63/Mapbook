3e49c59cab8f13b28f54aeacf806b8082e6b902b
mshen63 <mshen63@gatech.edu>
2022-01-04 21:02:04 -0500

    Small bug fixes

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -9,14 +9,14 @@ RegisterPage.getInitialProps = async ({ req, res }) => {
     const cookies = req ? req.headers.cookie : null;
     const currUser = await getCurrentUser(cookies).catch(() => null)
 
-    if (currUser != null) {
-        if (res) {
-            res.writeHead(301, { Location: urls.pages.app.map });
-            res.end();
-        } else {
-            return Router.replace(urls.pages.app.map);
-        }
-    }
+    // if (currUser != null) {
+    //     if (res) {
+    //         res.writeHead(301, { Location: urls.pages.app.map });
+    //         res.end();
+    //     } else {
+    //         return Router.replace(urls.pages.app.map);
+    //     }
+    // }
 
     return {currUser}
 }
